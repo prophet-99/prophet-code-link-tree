886106080c9d117ecd3a08a092e4b3f242c79af8
Alexander Avila <55512832+prophet-99@users.noreply.github.com>
2026-05-01 18:16:37 +0000

    feat: update links

--- src/constants/prophet-code-links.ts
+++ src/constants/prophet-code-links.ts
@@ -1,5 +1,5 @@
-const TIK_TOK_URL = "https://www.tiktok.com/@prophet.code";
-const YOUTUBE_URL = "https://www.youtube.com/@ProphetCode";
+const TIK_TOK_URL = "https://www.tiktok.com/@prophetcode.dev";
+const YOUTUBE_URL = "https://www.youtube.com/@prophetcode";
 const INSTAGRAM_URL = "https://www.instagram.com/prophetcode.dev";
 const TWITCH_URL = "https://www.twitch.tv/prophetcode";
 const ACADEMY_URL = "https://start.prophetcode.academy";
